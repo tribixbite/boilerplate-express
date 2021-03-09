c9463f8dcf938e4953b36f0177f40ff03d8f2378
Will <willstone@gmail.com>
2021-03-09 03:42:10 +0000

    first 6ish assignments

--- myApp.js
+++ myApp.js
@@ -2,24 +2,27 @@ var express = require('express');
 var app = express();
 
 
+app.use(function middleware(req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
+
+console.log("Hello World");
+pubPath = __dirname + "/public";
+indexPath = __dirname + "/views/index.html";
+app.use("/public", express.static(pubPath));
+jsonMessage = "Hello json";
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/", function(req, res) {
+  //res.send("Hello Express")
+  res.sendFile(indexPath);
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    jsonMessage = jsonMessage.toUpperCase();
+  }
+  //res.json({ message: jsonMessage });
+});
 
 
 
